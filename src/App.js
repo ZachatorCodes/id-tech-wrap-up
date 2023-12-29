cfc8d65fb0057b74f4aa60a907d23c72b537f117
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-28 19:23:27 -0700

    Set up controlled form using MUI components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import { Container, Paper, TextField, Typography } from "@mui/material";
+import {
+  Container,
+  FormControlLabel,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import { useState } from "react";
 
@@ -6,9 +14,9 @@ function App() {
   const [lessonInfo, setLessonInfo] = useState({
     name: "",
     topic: "",
-    nextLesson: "",
-    mainContent: "",
-    newStudent: false,
+    nextLesson: "For next time, we'll...",
+    mainContent: "Today...",
+    newStudent: true,
   });
 
   function handleChange(e) {
@@ -22,41 +30,76 @@ function App() {
 
   return (
     <div className="App">
-      <Container maxWidth="lg">
+      <Container maxWidth="md">
         <Paper elevation={3} sx={{ background: "#65a765" }}>
           <Typography padding="10px" margin="24px" align="center" variant="h2">
             Time For Wrap Up!
           </Typography>
         </Paper>
       </Container>
-    
-      <form>
-        <input
-          placeholder="Name"
-          name="name"
-          value={lessonInfo.name}
-          onChange={handleChange}
-        ></input>
-        <input
-          placeholder="Topic"
-          name="topic"
-          value={lessonInfo.topic}
-          onChange={handleChange}
-        ></input>
-        <br />
-        <textarea
-          placeholder="Main Content"
-          name="mainContent"
-          value={lessonInfo.mainContent}
-          onChange={handleChange}
-        ></textarea>
-        <textarea
-          placeholder="Next Lesson"
-          name="nextLesson"
-          value={lessonInfo.nextLesson}
-          onChange={handleChange}
-        ></textarea>
-      </form>
+      <Container maxWidth="lg">
+        <Paper sx={{ background: "#d7ffd7" }} elevation={3}>
+          <Box component="form" textAlign="center">
+            <TextField
+              value={lessonInfo.name}
+              onChange={handleChange}
+              name="name"
+              label="Name"
+              sx={{ margin: 2, width: "37.5%", background: "#ffffff" }}
+            ></TextField>
+            <TextField
+              value={lessonInfo.topic}
+              onChange={handleChange}
+              name="topic"
+              label="Topic"
+              sx={{ margin: 2, width: "37.5%", background: "#ffffff" }}
+            ></TextField>
+            <br />
+            <TextField
+              name="mainContent"
+              label="Main Content"
+              multiline
+              value={lessonInfo.mainContent}
+              onChange={handleChange}
+              rows={5}
+              sx={{ marginBottom: 2, width: "75%", background: "#ffffff" }}
+            ></TextField>
+            <TextField
+              value={lessonInfo.nextLesson}
+              onChange={handleChange}
+              name="nextLesson"
+              label="For Next Lesson..."
+              multiline
+              rows={5}
+              sx={{ marginBottom: 2, width: "75%", background: "#ffffff" }}
+            ></TextField>
+            <RadioGroup
+              background="#ffffff"
+              row
+              name="newStudent"
+              value={lessonInfo.newStudent}
+              onChange={handleChange}
+            >
+              <Container>
+                <FormControlLabel
+                  value={true}
+                  control={<Radio />}
+                  label="New Student"
+                  sx={{ marginBottom: 2 }}
+                  labelPlacement="top"
+                />
+                <FormControlLabel
+                  value={false}
+                  control={<Radio />}
+                  label="Returning Student"
+                  sx={{ marginBottom: 2 }}
+                  labelPlacement="top"
+                />
+              </Container>
+            </RadioGroup>
+          </Box>
+        </Paper>
+      </Container>
       <p>
         {lessonInfo.name} did an amazing job today! Since I haven't seen{" "}
         {lessonInfo.name} in a little while, we started the session off by
@@ -65,8 +108,7 @@ function App() {
         continue working on {lessonInfo.topic}! {lessonInfo.mainContent}{" "}
         Overall, I would say that it was an awesome session, and{" "}
         {lessonInfo.name} did a great job absorbing and applying all of the
-        concepts that we covered today. For next time, we'll{" "}
-        {lessonInfo.nextLesson}
+        concepts that we covered today. {lessonInfo.nextLesson}
       </p>
     </div>
   );
