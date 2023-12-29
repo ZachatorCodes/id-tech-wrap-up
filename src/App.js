9ac3f279e4381bfc492b1d15ff2b143445999f04
Zach Schwartz <zachatorcodes@gmail.com>
2023-12-28 22:15:50 -0700

    Move everything into components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useState } from "react";
 import AOrAnd from "./components/AOrAnd";
 import StudentForm from "./components/StudentForm";
 import Header from "./components/Header";
+import WrapUp from "./components/WrapUp";
 
 function App() {
   const [lessonInfo, setLessonInfo] = useState({
@@ -23,27 +24,19 @@ function App() {
     newStudent: true,
   });
 
-  
-
   return (
     <div className="App">
       <Header />
-      <StudentForm lessonInfo={lessonInfo} setLessonInfo={setLessonInfo}/>
-      <Container>
-        <Paper>
-          <Typography padding="10px" margin="24px" variant="h5">
-            {lessonInfo.name} did <AOrAnd firstLetter={lessonInfo.howDo[0]} />{" "}
-            {lessonInfo.howDo} job today! Since I haven't seen {lessonInfo.name}{" "}
-            in a little while, we started the session off by catching up a
-            little bit with an icebreaker activity. It's always great to hear
-            how {lessonInfo.name} is doing! After that, we hopped on over to
-            continue working on {lessonInfo.topic}! {lessonInfo.mainContent}{" "}
-            Overall, I would say that it was an awesome session, and{" "}
-            {lessonInfo.name} did a great job absorbing and applying all of the
-            concepts that we covered today. {lessonInfo.nextLesson}
-          </Typography>
-        </Paper>
-      </Container>
+      <StudentForm lessonInfo={lessonInfo} setLessonInfo={setLessonInfo} />
+      <WrapUp
+        name={lessonInfo.name}
+        firstLetter={lessonInfo.howDo[0]}
+        howDo={lessonInfo.howDo}
+        topic={lessonInfo.topic}
+        nextLesson={lessonInfo.nextLesson}
+        mainContent={lessonInfo.mainContent}
+        newStudent={lessonInfo.newStudent}
+      />
     </div>
   );
 }
