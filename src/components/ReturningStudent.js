c46a24103f7af50b78a2889657bb2bae6ecc3c5c
Zach Schwartz <zachatorcodes@gmail.com>
2024-01-01 16:42:30 -0700

    Create component for returning student wrap up

--- src/components/ReturningStudent.js
+++ src/components/ReturningStudent.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
-function ReturningStudent() {
+function ReturningStudent({
+  name,
+  topic,
+  howDo,
+  mainContent,
+  nextLesson,
+  preposition,
+}) {
   return (
-    <div>ReturningStudent</div>
-  )
+    <Typography padding="10px" margin="24px" variant="h5">
+      {name} did {preposition} {howDo} job today! Since I haven't seen {name} in
+      a little bit, we started the session out with a little icebreaker activity
+      to catch up. It's always great to hear how {name} is doing! After that, we
+      hopped on over to continue working on {topic}. {mainContent} Overall, I
+      would say that it was {preposition} {howDo} session, and {name} did a
+      great job absorbing and applying all of the concepts that we covered
+      today! {nextLesson}
+    </Typography>
+  );
 }
 
-export default ReturningStudent
+export default ReturningStudent;
