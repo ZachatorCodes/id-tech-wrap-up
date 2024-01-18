7dea0d938867b06c683187cc5ad6c348a73b4b3d
Zach <zachatorcodes@gmail.com>
2024-01-18 16:41:55 -0700

    Style buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,7 +5,7 @@ function Buttons() {
   return (
     <Container maxWidth="lg" disableGutters sx={{paddingLeft: 2, paddingRight: 2}}>
       <Paper elevation={0} align="center" className='paper-group'>
-        <Button sx={{margin: 1}}>Copy Wrap Up</Button><Button sx={{margin: 1}}>Show Wrap Up</Button>
+        <Button sx={{marginRight: 2, marginTop: 1, marginBottom: 1}}>Copy Wrap Up</Button><Button sx={{marginLeft: 2, marginTop: 1, marginBottom: 1}}>Show Wrap Up</Button>
       </Paper>
     </Container>
   )
