95eaa78e35ddd4609ce5e5c5cec9fd9bbb514e51
Zach <zachatorcodes@gmail.com>
2024-01-18 16:53:23 -0700

    Finish customizing buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,14 +1,28 @@
-import { Button, Container, Paper } from '@mui/material'
-import React from 'react'
+import { Button, Container, Paper } from "@mui/material";
+import React from "react";
 
 function Buttons() {
   return (
-    <Container maxWidth="lg" disableGutters sx={{paddingLeft: 2, paddingRight: 2}}>
-      <Paper elevation={0} align="center" className='paper-group'>
-        <Button sx={{marginRight: 2, marginTop: 1, marginBottom: 1}}>Copy Wrap Up</Button><Button sx={{marginLeft: 2, marginTop: 1, marginBottom: 1}}>Show Wrap Up</Button>
-      </Paper>
+    <Container
+      align="center"
+      maxWidth="xs"
+      disableGutters
+      sx={{ paddingLeft: 2, paddingRight: 2 }}
+    >
+        <Button
+          variant="contained"
+          sx={{ marginRight: 2, marginTop: 1, marginBottom: 1 }}
+        >
+          Copy Wrap Up
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ marginLeft: 2, marginTop: 1, marginBottom: 1 }}
+        >
+          Show Wrap Up
+        </Button>
     </Container>
-  )
+  );
 }
 
-export default Buttons
+export default Buttons;
