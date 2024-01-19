74ca64cdab08850385b52eb125617480af9a18c6
Zach <zachatorcodes@gmail.com>
2024-01-18 17:55:48 -0700

    Readd button for copy

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,9 +6,9 @@ function Buttons({ showWrapUp, setShowWrapUp }) {
     setShowWrapUp(!showWrapUp);
   }
 
-  // function handleCopyButton() {
-  //   console.log("Clicked")
-  // }
+  function handleCopyButton() {
+    console.log("Clicked")
+  }
 
   return (
     <Container
@@ -18,16 +18,16 @@ function Buttons({ showWrapUp, setShowWrapUp }) {
       sx={{ paddingLeft: 2, paddingRight: 2, paddingBottom: 2 }}
     >
       <Paper className="paper-group" elevation={0}>
-      {/* <Button
+      <Button
         variant="contained"
         sx={{ marginRight: 2, marginTop: 1.5, marginBottom: 1.5 }}
         onClick={handleCopyButton}
       >
         Copy Wrap Up
-      </Button> */}
+      </Button>
       <Button
         variant="contained"
-        sx={{ marginTop: 1.5, marginBottom: 1.5 }}
+        sx={{ marginLeft: 2, marginTop: 1.5, marginBottom: 1.5 }}
         onClick={handleToggleWrapUp}
       >
         {showWrapUp ? "Hide Wrap Up" : "Show Wrap Up"}
