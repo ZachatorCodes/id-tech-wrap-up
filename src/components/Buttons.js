2eefd90d9ed0650f3b97379b214c839ed4ed12f0
Zach <zachatorcodes@gmail.com>
2024-01-18 18:26:33 -0700

    Add ability to copy text to clipboard

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,7 +7,8 @@ function Buttons({ showWrapUp, setShowWrapUp }) {
   }
 
   function handleCopyButton() {
-    console.log("Clicked")
+    const wrapUpText = document.getElementById("wrap-up").textContent;
+    navigator.clipboard.writeText(wrapUpText)
   }
 
   return (
@@ -18,20 +19,20 @@ function Buttons({ showWrapUp, setShowWrapUp }) {
       sx={{ paddingLeft: 2, paddingRight: 2, paddingBottom: 2 }}
     >
       <Paper className="paper-group" elevation={0}>
-      <Button
-        variant="contained"
-        sx={{ marginRight: 2, marginTop: 1.5, marginBottom: 1.5 }}
-        onClick={handleCopyButton}
-      >
-        Copy Wrap Up
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ marginLeft: 2, marginTop: 1.5, marginBottom: 1.5 }}
-        onClick={handleToggleWrapUp}
-      >
-        {showWrapUp ? "Hide Wrap Up" : "Show Wrap Up"}
-      </Button>
+        <Button
+          variant="contained"
+          sx={{ marginRight: 2, marginTop: 1.5, marginBottom: 1.5 }}
+          onClick={handleCopyButton}
+        >
+          Copy Wrap Up
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ marginLeft: 2, marginTop: 1.5, marginBottom: 1.5 }}
+          onClick={handleToggleWrapUp}
+        >
+          {showWrapUp ? "Hide Wrap Up" : "Show Wrap Up"}
+        </Button>
       </Paper>
     </Container>
   );
