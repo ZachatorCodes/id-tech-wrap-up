8cf729ac2c162aadbe1b9988b270311bb44099ca
Zach <zachatorcodes@gmail.com>
2024-01-18 17:53:19 -0700

    Remove unusable button for deploy

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -18,16 +18,16 @@ function Buttons({ showWrapUp, setShowWrapUp }) {
       sx={{ paddingLeft: 2, paddingRight: 2, paddingBottom: 2 }}
     >
       <Paper className="paper-group" elevation={0}>
-      <Button
+      {/* <Button
         variant="contained"
         sx={{ marginRight: 2, marginTop: 1.5, marginBottom: 1.5 }}
         onClick={handleCopyButton}
       >
         Copy Wrap Up
-      </Button>
+      </Button> */}
       <Button
         variant="contained"
-        sx={{ marginLeft: 2, marginTop: 1.5, marginBottom: 1.5 }}
+        sx={{ marginTop: 1.5, marginBottom: 1.5 }}
         onClick={handleToggleWrapUp}
       >
         {showWrapUp ? "Hide Wrap Up" : "Show Wrap Up"}
