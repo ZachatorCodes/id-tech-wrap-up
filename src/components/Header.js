e1541027ce459d2fd701f4d48d14a238b1c9fd27
Zach <zachatorcodes@gmail.com>
2024-01-18 17:29:09 -0700

    Adjust elevation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Container, Paper, Typography } from "@mui/material";
 function Header() {
   return (
     <Container maxWidth="md">
-      <Paper elevation={3} sx={{ background: "#65a765" }}>
+      <Paper elevation={10} sx={{ background: "#65a765" }}>
         <Typography padding="10px" margin="24px" align="center" variant="h2">
           Time For Wrap Up!
         </Typography>
