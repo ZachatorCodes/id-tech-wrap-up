730a31f7a82d1b8bedbe496f820a6b54599527a9
Zach <zachatorcodes@gmail.com>
2024-01-18 18:55:23 -0700

    Fix margin error

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -28,14 +28,14 @@ function Buttons({ showWrapUp, setShowWrapUp }) {
       <Paper className="paper-group" elevation={0}>
         <Button
           variant="contained"
-          sx={{ marginRight: 2, marginTop: 1.5, marginBottom: 1.5 }}
+          sx={{ margin: 2 }}
           onClick={handleCopyButton}
         >
           Copy Wrap Up
         </Button>
         <Button
           variant="contained"
-          sx={{ marginLeft: 2, marginTop: 1.5, marginBottom: 1.5 }}
+          sx={{ margin: 2 }}
           onClick={handleToggleWrapUp}
         >
           {showWrapUp ? "Hide Wrap Up" : "Show Wrap Up"}
